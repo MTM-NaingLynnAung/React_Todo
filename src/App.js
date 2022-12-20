b500a08f476813bfa9c79a4d40dd6e7dbbb6dbf8
NaingLynnAung <scm.nainglynnaung@gmail.com>
2022-12-20 17:04:37 +0630

    create todo and show list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { useState } from 'react';
+const App = () => {
+  
+  const [ titleName, setValue ] = useState('');
+  const [ todos, setTodos ] = useState([]);
+  const addTodo = () => {
+    let val = titleName.trim();
+    if(val){
+      setTodos([...todos, { id: todos.length + 1, title: titleName }]);
+      setValue('');
+    }
+   }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div className='col-3 m-auto'>
+        <div>
+          <input type="text" className='form-control' onKeyPress={ event => event.charCode === 13 && addTodo() } onChange={ (event) => { setValue(event.target.value) } } value={titleName} />
+        </div>
+        <ul className='mt-3 pt-3'>
+          { todos.map((todo) => {
+              return (
+                <li className='d-flex align-items-center position-relative mb-4' key={todo.id}>
+                  <input type="checkbox" className='me-5 larger' />
+                  <span className='text-size'>{ todo.title }</span>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    
+      
     </div>
   );
+  
 }
 
 export default App;
