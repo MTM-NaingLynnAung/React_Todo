c8c70d9d4532b7be9013c60991ba79a3552b1467
NaingLynnAung <scm.nainglynnaung@gmail.com>
2022-12-21 17:00:17 +0630

    todo edit and delete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 const App = () => {
-  
+  const inputRef = useRef(null)
   const [ titleName, setValue ] = useState('');
   const [ todos, setTodos ] = useState([]);
+  let [ editTitle, setEditValue ] = useState('');
+
   const addTodo = () => {
     let val = titleName.trim();
     if(val){
-      setTodos([...todos, { id: todos.length + 1, title: titleName }]);
+      setTodos([...todos, { id: todos.length + 1, title: titleName, completed: false, edit: false }]);
       setValue('');
     }
+   }
+
+   const handleComplete = (todo) => {
+    setTodos(
+      todos.map(item => {
+        if(item.id === todo.id){
+          return { ...item, completed: !item.completed }
+        }
+        return item
+      })
+    )
+   }
+
+   const handleEdit = (todo) => {
+        console.log('outside')
+    setEditValue(todo.title)
+    setTodos(
+      todos.map(item => {
+        if(item.id === todo.id){
+          return { ...item, title: editTitle, edit: !item.edit }
+        }
+        return item
+      })
+    )
+   }
+
+   const handleDelete = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
    }
   
   return (
@@ -22,9 +52,14 @@ const App = () => {
         <ul className='mt-3 pt-3'>
           { todos.map((todo) => {
               return (
-                <li className='d-flex align-items-center position-relative mb-4' key={todo.id}>
-                  <input type="checkbox" className='me-5 larger' />
-                  <span className='text-size'>{ todo.title }</span>
+                <li className='d-flex align-items-center position-relative mb-4 li-width' key={todo.id}>
+                  <input type="checkbox" className='me-5 larger' onClick={() => handleComplete(todo)} />
+                  <div className="w-100 h-100 d-flex align-items-center" onDoubleClick={ () => handleEdit(todo) } onKeyPress={ (e) => e.charCode === 13 && handleEdit(todo) }  >
+                    <label className={ `text-size w-100 ${ todo.completed === true ? 'complete' : '' } `} >{ todo.title }</label>
+                    <span className="position-absolute size" onClick={() => handleDelete(todo.id)} >&times;</span>
+                    <input ref={inputRef} type={todo.edit ? 'text' : 'hidden'} id="edit" className="edit-form text-size" onChange={ (event) => { setEditValue(event.target.value) } } value={editTitle} onBlur={ () => handleEdit(todo)} />
+                  </div>
+                  
                 </li>
               )
             })
